49f27c2cc74e8f5ea3418337ad2af3b9d7231579
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-25 19:48:32 +0530

    send message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,53 @@
+import { Container, TextField, Typography, Button } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
 import {io} from 'socket.io-client'
 //this would be responsible for connection client with the server
 
 function App() {
-  const socket = io('http://localhost:3000');
+  const socket = useMemo(()=> io('http://localhost:3000'), [])
+
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", message)
+  }
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected to the server', socket.id);
+    })
+
+    socket.on("message", (data) => {
+      console.log(data)
+    })
+
+    socket.on("welcome", (msg) => {
+      console.log(msg)
+    })
+
+    return ()=> {
+      socket.disconnect()
+    }
+
+  }, [])
+
   return (
     
     <>
-      <div>
-        <h1>Hello World....</h1>
-      </div>
+      <Container>
+        <Typography variant='h1' component="div" gutterBottom>
+          Welcome to the chat application built by - Rohit
+        </Typography>
+      </Container>
+
+      <form onSubmit={handleSubmit}>
+        <TextField type='text' value={message} onChange={(e)=> setMessage(e.target.value)}/>
+        <Button type='submit' variant="contained" color="primary">Send</Button>
+      </form>
     </>
+
+    
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -25,8 +25,20 @@ app.get("/", (req, res) => {
 
 //this is a socket function that establishes a live connection
 io.on('connection', (socket) => {
-    console.log('a new client has connected')
-    console.log("with id: ", socket.id)
+    console.log('a new client has connected with id : ', socket.id)
+
+
+    socket.on("message", (data)=> {
+        console.log(data);
+        io.emit("message", data);
+    })
+    // socket.emit("welcome", "Welcome to our chat application")
+
+    // socket.broadcast.emit("welcome", `${socket.id} has joined the chat`)
+
+    socket.on("disconnect", ()=> {
+        console.log("a client has disconnected", socket.id)
+    })
 })
 
 
